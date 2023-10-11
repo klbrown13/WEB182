a67be3599282497ea9cf84d971437e1e7b85477a
Kurt Brown <128055241+klbrown13@users.noreply.github.com>
2023-10-11 19:03:38 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- Kurt Brown
+     Web 182
+     9/13/2023 -->
+
+
+<?php include 'view/header.php'; ?>
+<link rel="stylesheet" type="text/css"
+          href="main.css">
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
